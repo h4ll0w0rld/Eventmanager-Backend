8fc07c595fe843ea3b51f4475425737527145555
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2023-11-15 16:05:51 +0100

    DB Config
    Changed DB  config for server testings

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,9 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'dev',
-    DB: 'ventIT',
+    HOST: '127.0.0.1',
+    USER: 'eventuser',
+    PASSWORD: 'Event123SQL1!',
+    DB: 'Planit',
+    // DB: 'eventappdb',
     PORT: 3306,
     DIALECT: 'mysql',
     TIMEZONE: '+00:00'
