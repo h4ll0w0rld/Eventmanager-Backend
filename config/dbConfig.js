e5f57b44c2df96e928c0dbf0e5f49d398fd7f1b1
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2023-11-15 16:20:47 +0100

    PW Fix

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: '127.0.0.1',
     USER: 'eventuser',
-    PASSWORD: 'Event123SQL1!',
+    PASSWORD: 'Event123SQL1',
     DB: 'Planit',
     // DB: 'eventappdb',
     PORT: 3306,
