ffdad71da7e33a7fbe60e6c63f42876d01fd158f
YonyM <80950518+YonyM@users.noreply.github.com>
2023-12-14 17:04:02 +0100

    [FIX]

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: '127.0.0.1',
-    USER: 'root',
-    PASSWORD: 'J0174th4171998',
-    DB: 'testDB',
+    USER: 'eventuser',
+    PASSWORD: 'Event123SQL1',
+    DB: 'Planit',
     // DB: 'eventappdb',
     PORT: 3306,
     DIALECT: 'mysql',
