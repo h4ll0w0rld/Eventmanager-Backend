7a45baf224ed6eafd2e0d977cf95d7bef5fdd1a4
Florian Gaupp <florian.gaupp@hs-furtwangen.de>
2024-01-04 16:33:51 +0100

    [EDIT] change permission for getAllUserByEvent to checkUser

--- src/routes/api/event_router.js
+++ src/routes/api/event_router.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/add', eventController.addEvent);
-router.get('/:current_event_id/allUsersByEvent', checkRole, permissionMiddleware.checkAdmin, eventController.getAllUsersByEvent);
+router.get('/:current_event_id/allUsersByEvent', checkRole, permissionMiddleware.checkUser, eventController.getAllUsersByEvent);
 router.delete('/:current_event_id/delete', checkRole, permissionMiddleware.checkAdmin, eventController.deleteEventById);
 router.get('/:current_event_id/addUserToEvent/user_id/:user_id', checkRole, permissionMiddleware.checkAdmin, eventController.addUserToEvent);
 router.get('/:current_event_id/removeUserFromEvent/user_id/:user_id', checkRole, permissionMiddleware.checkCurrentUserOrAdmin, eventController.removeUserFromEvent);
